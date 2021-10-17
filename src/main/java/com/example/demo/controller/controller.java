2ef08ca7e74629e69cb094e744427cace141be1e
juan <juandavidcamargo99@gmail.com>
2021-10-16 19:39:57 -0500

    creacion de formulario usuarios

--- src/main/java/com/example/demo/controller/controller.java
+++ src/main/java/com/example/demo/controller/controller.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.dao.UsuarioDao;
 import com.example.demo.dao.ClienteDao;
@@ -19,25 +20,25 @@ import com.example.demo.dto.Cliente;
 
 @Controller
 public class controller {
-	
+
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
-	
+
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
-	
+
 	@PostMapping("/login")
 	public ModelAndView loginpost(HttpServletRequest request, HttpSession session) {
-		UsuarioDao Dao=new UsuarioDao();
+		UsuarioDao Dao = new UsuarioDao();
 		ModelAndView mav = new ModelAndView();
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		if(Dao.login(username, password)) {
-			session.setAttribute("login", '1');			
+		if (Dao.login(username, password)) {
+			session.setAttribute("login", '1');
 		} else {
 			mav.addObject("error", "Usuario o contraseña invalidas");
 			mav.setViewName("login");
@@ -45,18 +46,49 @@ public class controller {
 		}
 		return new ModelAndView("redirect:/listar-clientes");
 	}
-	
+
 	@PostMapping("/logout")
 	public ModelAndView logout(Model model, HttpSession session) {
 		session.removeAttribute("login");
 		return new ModelAndView("redirect:/login");
 	}
-	
-	
-	
+
+	@GetMapping("/crear-usuario")
+	public String crearUsuario(Model model) {
+		return "crearUsuario";
+	}
+
+	@PostMapping("/crear-usuario")
+	public String crearUsuarioPost(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		String name = request.getParameter("name");
+		String lastName = request.getParameter("lastname");
+		String accountName = request.getParameter("accountName");
+		String password = request.getParameter("password");
+		UsuarioDao Dao = new UsuarioDao();
+		// buscar usuario por accountName
+		if (Dao.buscarUsuarioPorAccountName(accountName)) {
+			redirectAttributes.addFlashAttribute("msg", "El usuario ya se encuentra registrado");
+		} else {
+			// crear usuario si no existe
+			if (Dao.crearUsuario(name, lastName, accountName, password)) {
+				redirectAttributes.addFlashAttribute("msg", "Usuario creado con exito");
+			} else {
+				redirectAttributes.addFlashAttribute("msg", "Usuario creado con exito");
+			}
+		}
+		return "redirect:/listar-usuarios";
+	}
+
+	@GetMapping("/listar-usuarios")
+	public String listarUsuarios(Model model) {
+		UsuarioDao Dao = new UsuarioDao();
+		model.addAttribute("usuarios", Dao.listarUsuarios());
+		return "listarUsuarios";
+	}
+
 	@GetMapping("/listar-clientes")
 	public String listarClientes(Model model) {
-		ClienteDao Dao=new ClienteDao();
+		ClienteDao Dao = new ClienteDao();
 		model.addAttribute("clientes", Dao.listaDeClientes());
 		return "ListarClientes";
 	}
--- src/main/java/com/example/demo/dao/UsuarioDao.java
+++ src/main/java/com/example/demo/dao/UsuarioDao.java
@@ -20,6 +20,7 @@ public class UsuarioDao {
 	 * @return boolean
 	 */
 	public boolean login(String name, String password) {
+		boolean result = false;
 		conexion = Conexion.conectar();
 		if (Conexion.AutoCommit(conexion)) {
 			try {
@@ -29,20 +30,106 @@ public class UsuarioDao {
 				consulta.setString(2, password);
 				ResultSet res = consulta.executeQuery();
 				if (res.next()) {
-					return true;
+					result = true;
 				}
 				res.close();
 				consulta.close();
-
 				Conexion.commit(conexion);
+				return result;
 			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, "no se pudo consultar la Persona\n" + e);
+				System.out.print(e.getMessage());
+				Conexion.rollback(conexion);
+			} finally {
+				Conexion.cerrar(conexion);
+			}
+		}
+		return result;
+	}
+	/**
+	 * permite crear usuarios
+	 * 
+	 * @return String
+	 */
+	public Boolean crearUsuario(String name, String lastName, String accountName, String password) {
+		conexion = Conexion.conectar();
+		if (Conexion.AutoCommit(conexion)) {
+			try {
+				String sql = "INSERT INTO usuario (name, lastName, accountName,  password) VALUES (?,?,?,?)";
+				PreparedStatement consulta = conexion.prepareStatement(sql);
+				consulta.setString(1, name);
+				consulta.setString(2, lastName);
+				consulta.setString(3, accountName);
+				consulta.setString(4, password);
+				consulta.executeUpdate();
+				consulta.close();
+				Conexion.commit(conexion);
+				return true;
+			} catch (Exception e) {
+				System.out.print(e.getMessage());
 				Conexion.rollback(conexion);
 			} finally {
 				Conexion.cerrar(conexion);
 			}
 		}
 		return false;
-	}
 
+	}
+	
+	/**
+	 * permite comprobar si existe un usuario ya creado, buscando por accountName
+	 */
+	public Boolean buscarUsuarioPorAccountName(String accountName) {
+		boolean result = false;
+		conexion = Conexion.conectar();
+		if (Conexion.AutoCommit(conexion)) {
+			try {
+				PreparedStatement consulta = conexion.prepareStatement("SELECT * FROM usuario WHERE accountName = ?");
+				consulta.setString(1, accountName);
+				ResultSet res = consulta.executeQuery();
+				if(res.next()) {
+					result = true;
+				}
+				res.close();
+				consulta.close();
+				Conexion.commit(conexion);
+			} catch (Exception e) {
+				System.out.print(e.getMessage());
+				Conexion.rollback(conexion);
+			} finally {
+				Conexion.cerrar(conexion);
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * 	permite listar usuarios
+	 */
+	public ArrayList<Usuario> listarUsuarios() {
+		ArrayList<Usuario> usuario = new ArrayList<Usuario>();
+		conexion = Conexion.conectar();
+		if (Conexion.AutoCommit(conexion)) {
+			try {
+				PreparedStatement consulta = conexion.prepareStatement("SELECT * FROM usuario");
+				ResultSet res = consulta.executeQuery();
+				while (res.next()) {
+					Usuario persona = new Usuario();
+					persona.setId(Integer.parseInt(res.getString("id")));
+					persona.setName(res.getString("name"));
+					persona.setLastName(res.getString("lastName"));
+
+					usuario.add(persona);
+				}
+				res.close();
+				consulta.close();
+				Conexion.commit(conexion);
+			} catch (Exception e) {
+				System.out.print(e.getMessage());
+				Conexion.rollback(conexion);
+			} finally {
+				Conexion.cerrar(conexion);
+			}
+		}
+		return usuario;
+	}
 }
